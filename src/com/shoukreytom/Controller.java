7d3d86a2871f6eb545c3fab1a728eb6508e8cbe2
shoukreytom <shoukreytom01@gmail.com>
2020-02-21 19:23:34 +0200

    initial commit

---
+++ src/com/shoukreytom/Controller.java
@@ -0,0 +1,15 @@
+package com.shoukreytom;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+public class Controller {
+
+    @FXML
+    public void reports() {
+        Stage stage = new Stage();
+
+    }
+
+
+}
